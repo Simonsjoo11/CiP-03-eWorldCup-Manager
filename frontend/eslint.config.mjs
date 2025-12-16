b1e1c6d6d85f96b46ec46d89b2904b29202e815c
Simon Sjöö <simon.sjoo@forefront.se>
2025-12-16 10:13:58 +0100

    fixed a typo in eslint configuration

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,7 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-config-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
